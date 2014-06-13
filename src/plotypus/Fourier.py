4feb2cf79a4830a8a8984211f59d10cdc2a21b7f
Earl Bellinger <earlbellinger@gmail.com>
2014-06-13 16:15:12 -0400

    Removed the unnecessary column of zeros

--- src/plotypus/Fourier.py
+++ src/plotypus/Fourier.py
@@ -3,18 +3,18 @@ import numpy
 class Fourier():
     def __init__(self, degree=3):
         self.degree = degree
-
+    
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X, y=None):
-        data = numpy.array(zip(numpy.array(X).T[0], range(len(X))))
+        data = numpy.array(list(zip(numpy.array(X).T[0], range(len(X)))))
         phase, order = data[data[:,0].argsort()].T
         coefficients = trigonometric_coefficient_matrix(phase, self.degree)
         return numpy.array([mag for (orig, mag) # Put back in original order
                             in sorted(zip(order, coefficients),
                                       key=lambda pair: pair[0])])
-
+    
     def get_params(self, deep):
         return {'degree': self.degree}
     
@@ -23,7 +23,7 @@ class Fourier():
             self.degree = params['degree']
 
 def trigonometric_coefficient_matrix(phases, degree):
-    return numpy.array([[numpy.cos((j+1)*numpy.pi*phases[i]) if j % 2
-                         else numpy.sin(j*numpy.pi*phases[i])
+    return numpy.array([[numpy.cos(numpy.pi*(j+1)*phases[i]) if j % 2 == 0
+                         else numpy.sin(numpy.pi*j*phases[i])
                          for j in range(2*degree)]
                         for i in range(len(phases))])
