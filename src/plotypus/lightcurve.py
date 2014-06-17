1d865aa22b9b9105d582dbb9ead523d2b0512ea0
Earl Bellinger <earlbellinger@gmail.com>
2014-06-17 13:48:39 -0400

    Robust outlier detection & prettier plots

--- src/plotypus/lightcurve.py
+++ src/plotypus/lightcurve.py
@@ -1,15 +1,25 @@
 import numpy
 from math import floor
 from os import path
-from .utils import make_sure_path_exists, get_signal, get_noise, colvec
+from .utils import make_sure_path_exists, get_signal, get_noise, colvec, mad
 from .periodogram import find_period, rephase, get_phase
-from .Fourier import Fourier
+from .preprocessing import Fourier
 from sklearn.linear_model import LassoCV
 from sklearn.pipeline import Pipeline
 from sklearn.grid_search import GridSearchCV
 import warnings
-import matplotlib as mpl
-mpl.use('Agg')
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import rcParams
+rcParams['axes.labelsize'] = 10
+rcParams['xtick.labelsize'] = 10
+rcParams['ytick.labelsize'] = 10
+rcParams['legend.fontsize'] = 10
+rcParams['font.family'] = 'serif'
+rcParams['font.serif'] = ['Latin Modern']
+rcParams['text.usetex'] = True
+rcParams['figure.dpi'] = 300
+rcParams['savefig.dpi'] = 300
 import matplotlib.pyplot as plt
 
 __all__ = [
@@ -18,10 +28,10 @@ __all__ = [
     'plot_lightcurve'
 ]
 
-def get_lightcurve(filename, fourier_degree=15, cv=10,
+def get_lightcurve(filename, period=None, fourier_degree=15, cv=10,
                    min_period=0.2, max_period=32,
                    coarse_precision=0.001, fine_precision=0.0000001,
-                   sigma=5, min_phase_cover=2/3.,
+                   sigma=6, min_phase_cover=1/2.,
                    phases=numpy.arange(0, 1, 0.01), **options):
     
     # Initialize predictor
@@ -30,14 +40,15 @@ def get_lightcurve(filename, fourier_degree=15, cv=10,
     predictor = GridSearchCV(pipeline, params)
     
     # Load file
-    data = numpy.ma.masked_array(data=numpy.loadtxt(filename), mask=None)
+    data = numpy.ma.array(data=numpy.loadtxt(filename), mask=None, dtype=float)
     
     while True:
         # Find the period of the inliers
         signal = get_signal(data)
-        period = find_period(signal.T[0], signal.T[1], min_period, max_period,
-                             coarse_precision, fine_precision)
-        phase, mag, err = rephase(signal, period).T
+        _period = period if period is not None else \
+                  find_period(signal.T[0], signal.T[1], min_period, max_period,
+                              coarse_precision, fine_precision)
+        phase, mag, err = rephase(signal, _period).T
     
         # Determine whether there is sufficient phase coverage
         coverage = numpy.zeros((100))
@@ -58,11 +69,11 @@ def get_lightcurve(filename, fourier_degree=15, cv=10,
     
         # Reject outliers and repeat the process if there are any
         if sigma:
-            outliers = find_outliers(data.data, period, predictor, sigma)
+            outliers = find_outliers(data.data, _period, predictor, sigma)
             if set.issubset(set(numpy.nonzero(outliers.T[0])[0]),
                             set(numpy.nonzero(data.mask.T[0])[0])):
                 break
-            print("Rejecting", sum(outliers)[0], "outliers")
+            print("Flagging", sum(outliers)[0], "outliers")
             data.mask = numpy.ma.mask_or(data.mask, outliers)
     
     # Build light curve
@@ -71,46 +82,57 @@ def get_lightcurve(filename, fourier_degree=15, cv=10,
     # Shift to max light
     arg_max_light = lc.argmin()
     lc = numpy.concatenate((lc[arg_max_light:], lc[:arg_max_light]))
-    data.T[0] = numpy.array([get_phase(p, period, arg_max_light / 100.)
-                             for p in data.data.T[0]])
+    data.T[0] = numpy.fromiter((get_phase(p, _period, arg_max_light / 100.)
+                                for p in data.data.T[0]),
+                               numpy.float, len(data.data.T[0]))
     
-    return period, lc, data
+    return _period, lc, data
 
 def find_outliers(data, period, predictor, sigma):
     phase, mag, err = rephase(data, period).T
-    phase = numpy.resize(phase, (phase.shape[0], 1))
-    residuals = abs(predictor.predict(phase) - mag)
-    mse = numpy.array([0 if residual < error else (residual - error)**2
-                       for residual, error in zip(residuals, err)])
-    return numpy.tile(numpy.vstack(mse > sigma * mse.std()), data.shape[1])
+    residuals = numpy.absolute(predictor.predict(colvec(phase)) - mag)
+    outliers = residuals > sigma * mad(residuals[residuals > err])
+    return numpy.tile(numpy.vstack(outliers), data.shape[1])
 
-def plot_lightcurve(output, filename, lc, period, data,
-                    phases=numpy.arange(0, 1, 0.01),
-                    grid=True, invert=True):
+def plot_lightcurve(filename, lc, period, data, output='.', filetype='.png',
+                    legend=False, phases = numpy.arange(0, 1, 0.01)):
     ax = plt.gca()
-    ax.grid(grid)
-    if invert:
-        ax.invert_yaxis()
-    plt.xlim(-0.1,2.1)
+    ax.grid(True)
+    ax.invert_yaxis()
+    plt.xlim(0,2)
     
     # Plot the fitted light curve
-    plt.plot(numpy.hstack((phases,1+phases)), numpy.hstack((lc, lc)),
-             linewidth=1.5, color='r')
+    signal, = plt.plot(numpy.hstack((phases,1+phases)),
+                       numpy.hstack((lc, lc)),
+                       linewidth=1.5, color='black')
     
     # Plot points used
     phase, mag, err = get_signal(data).T
-    plt.errorbar(numpy.hstack((phase,1+phase)), numpy.hstack((mag, mag)),
-                 yerr = numpy.hstack((err,err)), ls='None', ms=.01, mew=.01)
+    inliers = plt.errorbar(numpy.hstack((phase,1+phase)),
+                            numpy.hstack((mag, mag)),
+                            yerr=numpy.hstack((err,err)),
+                            color='black', ls='None',
+                            ms=.01, mew=.01, capsize=0)
     
     # Plot outliers rejected
     phase, mag, err = get_noise(data).T
-    plt.errorbar(numpy.hstack((phase,1+phase)), numpy.hstack((mag, mag)),
-                 yerr = numpy.hstack((err,err)), ls='None', ms=.01, mew=.01,
-                 color='r')
+    outliers = plt.errorbar(numpy.hstack((phase,1+phase)),
+                             numpy.hstack((mag, mag)),
+                             yerr=numpy.hstack((err,err)),
+                             color='r', ls='None',
+                             ms=.01, mew=.01, capsize=0)
+    
+    if legend:
+        plt.legend([signal, inliers, outliers],
+                   ["Light Curve", "Inliers", "Outliers"],
+                   loc='best')
     
     plt.xlabel('Phase ({0:0.7} day period)'.format(period))
     plt.ylabel('Magnitude')
-    plt.title(filename.split('.')[0])
+    
+    name = filename.split('.')[0]
+    plt.title(name)
+    plt.tight_layout(pad=0.1)
     make_sure_path_exists(output)
-    plt.savefig(path.join(output, filename + '.png'))
+    plt.savefig(path.join(output, name + filetype))
     plt.clf()
