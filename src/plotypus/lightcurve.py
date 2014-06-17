a8f12633d584f203dc6b6453ffd3273ece4354b9
Earl Bellinger <earlbellinger@gmail.com>
2014-06-17 17:53:48 -0400

    Can now supply a file of periods

--- src/plotypus/lightcurve.py
+++ src/plotypus/lightcurve.py
@@ -1,138 +1,61 @@
 import numpy
-from math import floor
-from os import path
-from .utils import make_sure_path_exists, get_signal, get_noise, colvec, mad
-from .periodogram import find_period, rephase, get_phase
-from .preprocessing import Fourier
-from sklearn.linear_model import LassoCV
-from sklearn.pipeline import Pipeline
-from sklearn.grid_search import GridSearchCV
-import warnings
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import rcParams
-rcParams['axes.labelsize'] = 10
-rcParams['xtick.labelsize'] = 10
-rcParams['ytick.labelsize'] = 10
-rcParams['legend.fontsize'] = 10
-rcParams['font.family'] = 'serif'
-rcParams['font.serif'] = ['Latin Modern']
-rcParams['text.usetex'] = True
-rcParams['figure.dpi'] = 300
-rcParams['savefig.dpi'] = 300
-import matplotlib.pyplot as plt
+from sys import exit
+from os import path, listdir
+from optparse import OptionParser
+from plotypus.lightcurve import get_lightcurve, plot_lightcurve
 
-__all__ = [
-    'get_lightcurve',
-    'find_outliers',
-    'plot_lightcurve'
-]
+def get_ops():
+    parser = OptionParser()
+    parser.add_option('-i', '--input', type='string',
+        default=path.join('..', 'data', 'lmc', 'i', 'cep', 'f'),
+        help='location of stellar observations',)
+    parser.add_option('-o', '--output', type='string',
+        default=path.join('..', 'results'),
+        help='location of results')
+    parser.add_option('-p', '--periods', type='string',
+        default=None, help='file of star names and associated periods')
+    parser.add_option('--min_period', dest='min_period', type='float',
+        default=0.2, help='minimum period of each star')
+    parser.add_option('--max_period', dest='max_period', type='float',
+        default=32., help='maximum period of each star')
+    parser.add_option('--coarse_precision', dest='coarse_precision', type='int',
+        default=0.001, help='level of granularity on first pass')
+    parser.add_option('--fine_precision', dest='fine_precision', type='int',
+        default=0.0000001, help='level of granularity on second pass')
+    parser.add_option('--fourier_degree', dest='fourier_degree', type='int',
+        default=15, help='number of coefficients to generate')
+    parser.add_option('--sigma', dest='sigma', type='float',
+        default=6, help='rejection criterion for outliers')
+    parser.add_option('--cv',               dest='cv', type='int',
+        default=10, help='number of folds in the L1-regularization search')
+    parser.add_option('--min_phase_cover', dest='min_phase_cover', type='float',
+        default=1/2., help='minimum fraction of phases that must have points')
+    (options, args) = parser.parse_args()
+    return options
 
-def get_lightcurve(filename, period=None, fourier_degree=15, cv=10,
-                   min_period=0.2, max_period=32,
-                   coarse_precision=0.001, fine_precision=0.0000001,
-                   sigma=6, min_phase_cover=1/2.,
-                   phases=numpy.arange(0, 1, 0.01), **ops):
+def main():
+    ops = get_ops()
+    lcs = []
+    if ops.periods is not None:
+        periods = {name: float(period) for (name, period)
+                   in (line.split() for line
+                       in open(ops.periods, 'r') if ' ' in line)}
+    for filename in sorted(listdir(ops.input)):
+        name = filename.split('.')[0]
+        print(filename)
+        star = get_lightcurve(path.join(ops.input, filename),
+            period=periods[name] if name in periods else None,
+            **ops.__dict__)
     
-    # Initialize predictor
-    pipeline = Pipeline([('Fourier', Fourier()), ('Lasso', LassoCV(cv=cv))])
-    params = {'Fourier__degree': list(range(3, 1+fourier_degree))}
-    predictor = GridSearchCV(pipeline, params)
+        if star is not None:
+            period, lc, data = star
+            lcs += [[period] + list(lc)]
+            plot_lightcurve(filename, lc, period, data, **ops.__dict__)
     
-    # Load file
-    data = numpy.ma.array(data=numpy.loadtxt(filename), mask=None, dtype=float)
-    
-    while True:
-        # Find the period of the inliers
-        signal = get_signal(data)
-        _period = period if period is not None else \
-                  find_period(signal.T[0], signal.T[1], min_period, max_period,
-                              coarse_precision, fine_precision)
-        phase, mag, err = rephase(signal, _period).T
-    
-        # Determine whether there is sufficient phase coverage
-        coverage = numpy.zeros((100))
-        for p in phase:
-            coverage[int(floor(p*100))] = 1
-        if sum(coverage)/100. < min_phase_cover:
-            print(sum(coverage)/100., min_phase_cover)
-            print("Insufficient phase coverage")
-            return None
-    
-        # Predict light curve
-        with warnings.catch_warnings(record=True) as w:
-            try:
-                predictor = predictor.fit(colvec(phase), mag)
-            except Warning:
-                print(w)
-                return None
-    
-        # Reject outliers and repeat the process if there are any
-        if sigma:
-            outliers = find_outliers(data.data, _period, predictor, sigma)
-            if set.issubset(set(numpy.nonzero(outliers.T[0])[0]),
-                            set(numpy.nonzero(data.mask.T[0])[0])):
-                break
-            print("Flagging", sum(outliers)[0], "outliers")
-            data.mask = numpy.ma.mask_or(data.mask, outliers)
-    
-    # Build light curve
-    lc = predictor.predict([[i] for i in phases])
-    
-    # Shift to max light
-    arg_max_light = lc.argmin()
-    lc = numpy.concatenate((lc[arg_max_light:], lc[:arg_max_light]))
-    data.T[0] = numpy.fromiter((get_phase(p, _period, arg_max_light / 100.)
-                                for p in data.data.T[0]),
-                               numpy.float, len(data.data.T[0]))
-    
-    return _period, lc, data
+    numpy.savetxt(path.join(ops.output, 'lightcurves.dat'),
+                  numpy.array(lcs), fmt='%.5f',
+                  header='Period ' + \
+                         ' '.join(['Phase' + str(i) for i in range(100)]))
 
-def find_outliers(data, period, predictor, sigma):
-    phase, mag, err = rephase(data, period).T
-    residuals = numpy.absolute(predictor.predict(colvec(phase)) - mag)
-    outliers = residuals > sigma * mad(residuals[residuals > err])
-    return numpy.tile(numpy.vstack(outliers), data.shape[1])
-
-def plot_lightcurve(filename, lc, period, data, output='.', filetype='.png',
-                    legend=False, phases = numpy.arange(0, 1, 0.01), **ops):
-    ax = plt.gca()
-    ax.grid(True)
-    ax.invert_yaxis()
-    plt.xlim(0,2)
-    
-    # Plot the fitted light curve
-    signal, = plt.plot(numpy.hstack((phases,1+phases)),
-                       numpy.hstack((lc, lc)),
-                       linewidth=1.5, color='black')
-    
-    # Plot points used
-    phase, mag, err = get_signal(data).T
-    inliers = plt.errorbar(numpy.hstack((phase,1+phase)),
-                            numpy.hstack((mag, mag)),
-                            yerr=numpy.hstack((err,err)),
-                            color='black', ls='None',
-                            ms=.01, mew=.01, capsize=0)
-    
-    # Plot outliers rejected
-    phase, mag, err = get_noise(data).T
-    outliers = plt.errorbar(numpy.hstack((phase,1+phase)),
-                             numpy.hstack((mag, mag)),
-                             yerr=numpy.hstack((err,err)),
-                             color='r', ls='None',
-                             ms=.01, mew=.01, capsize=0)
-    
-    if legend:
-        plt.legend([signal, inliers, outliers],
-                   ["Light Curve", "Inliers", "Outliers"],
-                   loc='best')
-    
-    plt.xlabel('Phase ({0:0.7} day period)'.format(period))
-    plt.ylabel('Magnitude')
-    
-    name = filename.split('.')[0]
-    plt.title(name)
-    plt.tight_layout(pad=0.1)
-    make_sure_path_exists(output)
-    plt.savefig(path.join(output, name + filetype))
-    plt.clf()
+if __name__ == "__main__":
+    exit(main())
