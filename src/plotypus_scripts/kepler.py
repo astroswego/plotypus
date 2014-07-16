e81a76c65f2f63aeb1152593dd3119d6e307a16d
Earl Bellinger <earlbellinger@gmail.com>
2014-07-16 16:17:48 -0400

    It's good if it actually works... =P

--- src/plotypus_scripts/kepler.py
+++ src/plotypus_scripts/kepler.py
@@ -6,7 +6,7 @@ from plotypus.lightcurve import get_lightcurve_from_file, plot_lightcurve
 def main():
     filename = 'V715Cyg.dat'
     period = 1.
-    period, lc, data = get_lightcurve_from_file(filename, period, sigma=100)
+    period, lc, data, *c = get_lightcurve_from_file(filename, period, sigma=100)
     plot_lightcurve(filename, lc, period, data, filetype='.eps', legend=True)
 
 if __name__ == "__main__":
