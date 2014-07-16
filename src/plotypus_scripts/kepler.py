47289894ff6bee69e641c50a3251d0cc5b6e3141
Earl Bellinger <earlbellinger@gmail.com>
2014-07-16 13:28:00 -0400

    For testing the Kepler star

---
+++ src/plotypus_scripts/kepler.py
@@ -0,0 +1,13 @@
+import numpy
+from sys import exit
+from os import path, listdir
+from plotypus.lightcurve import get_lightcurve_from_file, plot_lightcurve
+
+def main():
+    filename = 'V715Cyg.dat'
+    period = 1.
+    period, lc, data = get_lightcurve_from_file(filename, period, sigma=100)
+    plot_lightcurve(filename, lc, period, data, filetype='.eps', legend=True)
+
+if __name__ == "__main__":
+    exit(main())
