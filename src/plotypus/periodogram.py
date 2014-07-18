cdf29a91f5f117aa77e8a3311fc7d8a8bb15cb6e
Earl Bellinger <earlbellinger@gmail.com>
2014-07-18 18:56:47 -0400

    Made rephase much faster... and much more obvious

--- src/plotypus/periodogram.py
+++ src/plotypus/periodogram.py
@@ -28,8 +28,7 @@ def LombScargle(time, scaled_mags, precision, min_period, max_period):
 
 def rephase(data, period=1, col=0):
     rephased = numpy.ma.copy(data)
-    rephased.T[col] = numpy.fromiter(map(lambda x: get_phase(x, period),
-                                         rephased.T[col]), dtype=float)
+    rephased.T[col] = get_phase(rephased.T[col], period)
     return rephased
 
 def get_phase(time, period=1, offset=0):
