827e343fa6bf6761131e9531bd388faf4be5b2c6
Earl Bellinger <earlbellinger@gmail.com>
2014-07-23 16:53:58 -0400

    Update lasso-ols-A.r

--- lasso-ols-A.r
+++ lasso-ols-A.r
@@ -5,33 +5,34 @@ ylim <- c(-0.001, 0.001)
 
 setEPS()
 postscript('ogle-i-A.eps', fonts=c("serif"), height=5, width=6.6875)
-par(mfrow=c(2,2), mar=c(0, 5, 4, 0), ps=15)
+par(mfrow=c(2,2), mar=c(0, 3, 3, 0), ps=10, family=c("serif"))
 plot(log10(lasso$Period), lasso$A_0 - baart$A_0, pch=3, ylim=ylim,
      xlab="log P", ylab="", xaxt="n", yaxt="n")
 abline(h=0)
 axis(2)
-mtext(side=2, text=expression(Lasso A[0] - Baart A[0]), line=3, cex=0.5)
+mtext(side=2, text="Lasso A0 - Baart A0", line=2, cex=1) #₀
 #"Lasso A₀ - Baart A₀"
-par(mar=c(0, 0, 4, 5))
+par(mar=c(0, 0, 3, 3))
 plot(log10(lasso$Period), lasso$A_1 - baart$A_1, pch=3, ylim=ylim,
      xlab="log P", ylab="", xaxt="n", yaxt="n")
 abline(h=0)
-mtext(side=4, text="Lasso A₁ - Baart A₁", line=3, cex=0.5)
-
-par(mar=c(4, 5, 0, 0))
+mtext(side=4, text="Lasso A1 - Baart A1", line=2, cex=1) #₁
+par(mar=c(3, 3, 0, 0))
 plot(log10(lasso$Period), lasso$A_2 - baart$A_2, pch=3, ylim=ylim,
-     xlab="log P", ylab="", yaxt="n")
+     xlab="", ylab="", yaxt="n")
 abline(h=0)
-mtext(side=2, text="Lasso A₂ - Baart A₂", line=3, cex=0.5)
-par(mar=c(4, 0, 0, 5))
+mtext(side=2, text="Lasso A2 - Baart A2", line=2, cex=1) #₂
+mtext(side=1, text="log P", line=2, cex=1)
+par(mar=c(3, 0, 0, 3))
 
 plot(log10(lasso$Period), lasso$A_3 - baart$A_3, pch=3, ylim=ylim,
-     xlab="log P", ylab="", yaxt="n")
+     xlab="", ylab="", yaxt="n")
 abline(h=0)
 axis(4)
-mtext(side=4, text="Lasso A₃ - Baart A₃", line=3, cex=0.5)
+mtext(side=1, text="log P", line=2, cex=1)
+mtext(side=4, text="Lasso A3 - Baart A3", line=2, cex=1) #₃
 
-title("Differences in Amplitude Coefficients Between Lasso and Baart", outer=TRUE, line=-3)
+title("Differences in Amplitude Coefficients Between Lasso and Baart", outer=TRUE, line=-2.7, font.main=1)
 dev.off()
 
 # par(mar=c(0, 5, 0, 0))
