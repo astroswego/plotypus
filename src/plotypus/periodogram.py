4374018fa0754cd0ea92070d1d7cfd7ecc054079
Rosnec <rosnec.dev@gmail.com>
2014-12-15 22:58:08 -0500

    Resolved division by zero warnings in CE

--- src/plotypus/periodogram.py
+++ src/plotypus/periodogram.py
@@ -3,6 +3,7 @@ from scipy.signal import lombscargle
 from math import modf
 from multiprocessing import Pool
 from functools import partial
+from plotypus import utils
 
 __all__ = [
     'find_period',
@@ -76,16 +77,46 @@ def new_CE(period, data, xbins=10, ybins=5):
     r = rephase(data, period)
     bins, *_ = np.histogram2d(r[:,0], r[:,1], [xbins, ybins], [[0,1], [0,1]])
     size = r.shape[0]
+
+    ## The following code was once more readable, but much slower.
+    ## Here is what it used to be:
+    ## -----------------------------------------------------------------------
+    ##    return np.sum((lambda p: p * np.log(np.sum(bins[i,:]) / size / p) \
+    ##                             if p > 0 else 0)(bins[i][j] / size)
+    ##                  for i in np.arange(0, xbins)
+    ##                  for j in np.arange(0, ybins)) if size > 0 else np.PINF
+    ## -----------------------------------------------------------------------
+
     if size > 0:
+        # bins[i,j] / size
         divided_bins = bins / size
-        column_summed_bins = np.sum(divided_bins, axis=0)
-        with np.errstate(divide='ignore', invalid='ignore'):
-            A = divided_bins * np.log(column_summed_bins / divided_bins)
-        A[bins <= 0] = 0
+        # indices where that is positive
+        # to avoid division by zero
+        arg_positive = divided_bins > 0
+
+        # array containing the sums of each column in the bins array
+        column_sums = np.sum(divided_bins, axis=0)
+        # array is repeated row-wise, so that it can be sliced by arg_positive
+        column_sums = np.repeat(np.reshape(column_sums, (1,-1)), xbins, axis=0)
+
+        # select only the elements in both arrays which correspond to a
+        # positive bin
+        select_divided_bins = divided_bins[arg_positive]
+        select_column_sums  = column_sums[arg_positive]
+
+        # initialize the result array
+        A = np.empty((xbins, ybins), dtype=float)
+        # store at every index [i,j] in A which corresponds to a positive bin:
+        # bins[i,j]/size * log(bins[i,:] / size / (bins[i,j]/size))
+        A[ arg_positive] = \
+            select_divided_bins * np.log(select_column_sums
+                                       / select_divided_bins)
+        # store 0 at every index in A which corresponds to a non-positive bin
+        A[~arg_positive] = 0
+        # return the summation
         return np.sum(A)
     else:
         return np.PINF
-
 
 def rephase(data, period=1, shift=0, col=0):
     rephased = np.ma.copy(data)
