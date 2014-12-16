c91e6cd8e5b6214e35a00b85588cf2327b737fe6
Rosnec <rosnec.dev@gmail.com>
2014-12-15 19:30:26 -0500

    Removed all unnecessary matrix transposes
    I did this with the intention of reducing run time, but in the end run
    time was not altered. However, I believe this at least reduces memory
    usage, as the transposed arrays need not be stored anywhere. Since it
    does not hurt performance, and I at least think it makes the code a
    little nicer, I'm going to commit it anyway. Now to tackle the *real*
    bottleneck.

--- src/plotypus/periodogram.py
+++ src/plotypus/periodogram.py
@@ -49,11 +49,33 @@ def conditional_entropy(data, precision, min_period, max_period,
     entropies = list(m(partial_job, periods))
     return periods[np.argmin(entropies)]
 
+def new_conditional_entropy(data, precision, min_period, max_period,
+                        xbins=10, ybins=5, period_jobs=1):
+    periods = np.arange(min_period, max_period, precision)
+    copy = np.ma.copy(data)
+    copy[:,1] = (copy[:,1]  - np.min(copy[:,1])) \
+       / (np.max(copy[:,1]) - np.min(copy[:,1]))
+    partial_job = partial(new_CE, data=copy, xbins=xbins, ybins=ybins)
+    m = map if period_jobs <= 1 else Pool(period_jobs).map
+    entropies = list(m(partial_job, periods))
+    return periods[np.argmin(entropies)]
+
+
 def CE(period, data, xbins=10, ybins=5):
     if period <= 0: return np.PINF
     r = rephase(data, period)
     bins, *_ = np.histogram2d(r.T[0], r.T[1], [xbins, ybins], [[0,1], [0,1]])
     size = len(r.T[1])
+    return np.sum((lambda p: p * np.log(np.sum(bins[i,:]) / size / p) \
+                             if p > 0 else 0)(bins[i][j] / size)
+                  for i in np.arange(0, xbins)
+                  for j in np.arange(0, ybins)) if size > 0 else np.PINF
+
+def new_CE(period, data, xbins=10, ybins=5):
+    if period <= 0: return np.PINF
+    r = rephase(data, period)
+    bins, *_ = np.histogram2d(r[:,0], r[:,1], [xbins, ybins], [[0,1], [0,1]])
+    size = r.shape[0]
     return np.sum((lambda p: p * np.log(np.sum(bins[i,:]) / size / p) \
                              if p > 0 else 0)(bins[i][j] / size)
                   for i in np.arange(0, xbins)
